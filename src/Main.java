48d4538a0b8bd2894a95fca91d5cd5f7dfc7ad4a
benciprian <ben27ciprian@gmail.com>
2023-10-07 17:14:06 +0300

    newmessage

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Hello world!, I`am Lucian");
+        System.out.println("Hello, Hello world!, I`am Lucian, I'am  Ciprian Florin! ");
     }
 }
