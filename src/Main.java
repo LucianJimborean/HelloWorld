1421d899865116ea1843a89e292e0f3ade0d88a7
benciprian <ben27ciprian@gmail.com>
2023-10-07 16:46:16 +0300

    second 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! New World!");
+        System.out.println("Hello world! New World! Old Message Hello World!");
     }
 }
