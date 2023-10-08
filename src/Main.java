9cb312dfdc9848bf2bdb4b5276c8cf7086e82311
benciprian <ben27ciprian@gmail.com>
2023-10-08 19:37:08 +0300

    second 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Hello world!, I am your father");
+        System.out.println("Hello, Hello world!, I am your father, I am Ciprian");
     }
 }
